181b549bd4c139df7694b0f26062ef865931a951
SaulJCer <190316056+SaulJCer@users.noreply.github.com>
2026-05-18 18:18:59 -0400

    made it so it adds books to library array
    Also added 4 more books to the library

--- index.js
+++ index.js
@@ -14,21 +14,26 @@ function addBook(title, author, pages, read) {
 }
 
 
-function addNewBook (nameOfBook, author, pages, read) {
+function addNewBook (varName, nameOfBook, author, pages, read) {
+
+
 
     const book1 = new addBook(nameOfBook, author, pages, read);
-
-
-
-    myLibrary.push(book1);
-    return book1;
-
+    myLibrary.push(book1)
 
 }
 
+// function addBookToLibrary(bookObj) {
 
-// const theHobbit = new addBook("The Hobbit", "J.R.R. Tolkien", 295, "not read");
+//     book1 = bookObj;
+//     myLibrary.push(bookObj);
+// }
 
+const theHobbit = new addNewBook("The Hobbit", "J.R.R. Tolkien", 295, "not read");
+const theChicken = new addNewBook("The Chicken", "K.F.C. Nugget", 20, "not read");
+const arroz = new addNewBook("Arroz", "con pollo", 350, "not read");
+const ofMiceAndMen = new addNewBook("Of Mice and Men", "IDK", 148, "read");
+const theHungerGames = new addNewBook("The Hunger Games", "Susainze", 150, "read");
 
 function Chicken(recipe, cost) {
     this.recipe = recipe;
