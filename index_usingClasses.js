ae20cd237ae05ba89b220b7c0bd790af636b6b4e
SaulJCer <190316056+SaulJCer@users.noreply.github.com>
2026-06-24 21:12:22 -0400

    Added new js file that I will change by using classes

---
+++ index_usingClasses.js
@@ -0,0 +1,135 @@
+const myLibrary = [];
+
+
+
+function addBook(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.id = crypto.randomUUID();
+
+
+
+}
+
+
+function addNewBook (nameOfBook, author, pages, read) {
+
+
+
+    const book1 = new addBook(nameOfBook, author, pages, read);
+    myLibrary.push(book1)
+    return(book1);
+}
+
+// function addBookToLibrary(bookObj) {
+
+//     book1 = bookObj;
+//     myLibrary.push(bookObj);
+// }
+
+const theHobbit = new addNewBook("The Hobbit", "J.R.R. Tolkien", 295, "not read");
+const theChicken = new addNewBook("The Chicken", "K.F.C. Nugget", 20, "not read");
+const arroz = new addNewBook("Arroz", "con pollo", 350, "not read");
+const ofMiceAndMen = new addNewBook("Of Mice and Men", "IDK", 148, "read");
+const theHungerGames = new addNewBook("The Hunger Games", "Susainze", 150, "read");
+
+function deleteElementById(id) {
+    const element = document.getElementById(id);
+    if (element) {
+        element.remove();
+    }
+}
+
+
+function readCheck(bookRead) {
+    if(bookRead == "read") {
+        return "not read";
+    }
+    else {
+        return "read";
+    }
+}
+
+function addBookToSite(book) {
+    const newPara = document.createElement('div');
+    newPara.textContent = `
+     Title: ${book.title}
+     Author: ${book.author}
+     pages: ${book.pages}
+     Read: ${book.read}
+     ID: ${book.id}
+    `;
+
+    newPara.id = book.id;
+    const newButton = document.createElement('button');
+    newButton.id = `${book.id}`;
+    newButton.classList.add("RemoveBookButton")    
+    newButton.textContent = 'X';
+
+    const readButton = document.createElement("button");
+    readButton.id = "read"; 
+    readButton.textContent = book.read;
+    
+    readButton.addEventListener('click', () => {
+        console.log(book.read);
+        book.read = readCheck(book.read)
+        console.log(book.read);
+        readButton.textContent = book.read;
+    })
+
+
+    newButton.addEventListener('click', () => {
+        newPara.remove();
+        newButton.remove();
+        readButton.remove();
+    })
+
+    const htBook = document.getElementById("books");
+    
+    htBook.appendChild(newPara);
+    htBook.appendChild(newButton);
+    htBook.appendChild(readButton);
+}
+
+
+for (const book of myLibrary) {
+    addBookToSite(book);
+}
+
+
+const dialogF = document.querySelector('#newBookForm')
+const myForm = document.getElementById('newBookFromUser');
+
+const allowedValueforRead = ['read', 'not read', 'Read', 'Not read'];
+
+myForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const data = new FormData(e.target);
+
+
+    const titleValue = data.get("title");
+    const authorValue = data.get('author');
+    const pagesValue = data.get('pages');
+    const readValue = data.get("read");
+
+    if(!allowedValueforRead.includes(readValue)) {
+        alert("Pleast type: read or not read");
+        event.preventDefault();
+        return;
+    }
+
+
+
+
+    // pass values into form 
+
+    addBookToSite (addNewBook(titleValue, authorValue, pagesValue, readValue));
+
+
+    myForm.reset();
+    dialogF.close();
+});
+
+
